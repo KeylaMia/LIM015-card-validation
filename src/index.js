e72920d5e0088c0d6ee64f228518f446d47ead7b
KeylaMia <keylamialujimenezg@gmail.com>
2021-06-24 09:52:46 -0500

    subir todos mis cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-import validator from './validator.js';
+import validator from './validator.js'
+//console.log(validator);
+let creditCard= document.getElementById("creditNumber");
+const validar= document.getElementById('btnValida'); 
+let btnVolver= document.getElementById("volver");
+ const formulario= document.getElementById("formulario");
+formulario.creditNumber.addEventListener('keyup', (e) => {
+	let valorCreditNumber = e.target.value;
 
-console.log(validator);
+	formulario.creditNumber.value = valorCreditNumber
+	// Eliminamos espacios en blanco
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '')
+	// Ponemos espacio cada cuatro numeros
+	//.replace(/([0-9]{4})/g, '$1 ')
+	// Elimina el ultimo espaciado
+	.trim();
+})
+    function mostrarOcultar(){
+        console.log(creditCard.value);
+     if(validator.isValid(creditCard.value)==true){
+          document.getElementById('contenedor').style.display='none';
+          document.getElementById('correctValidation').style.display='block';
+     }else{
+          document.getElementById('contenedor').style.display='none';
+          document.getElementById('wrongvalidation').style.display='block';
+          }
+     }
+     validar.addEventListener("click", mostrarOcultar);
+
+function volver (){
+     if(document.getElementById("correctValidation")){
+          document.getElementById("correctValidation").style.display="none";
+          document.getElementById("contenedor").style.display="block";
+     }else{
+          document.getElementById("correctValidation").style.display="none";
+          document.getElementById("contenedor").style.display="block";
+     }
+     
+}
+
+btnVolver.addEventListener("click", volver);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
-const validator = {
-  // ...
-};
+const validator= {
+  
+  isValid: function(creditCard){
+    console.log(typeof creditCard);
+    let creditNumber= creditCard.split("");
+    let invertirCreditNumber = creditNumber.reverse();
+
+    let suma=0;
+  
+    for(let i = 0; i<invertirCreditNumber.length;i++){
+      //para las posiciones pares
+      let numInverter= parseInt(invertirCreditNumber[i]);
+     
+      if(i%2!=0){
+        numInverter=numInverter*2;
+        
+        if(numInverter>9){
+          numInverter= numInverter-9;
+           //menos 9
+        }else{
+          numInverter=parseInt(numInverter);
+          
+        }
+      } else{ 
+        numInverter;
+        console.log(numInverter);
+    
+      }
+      suma=suma+numInverter;
+
+    }
+
+  return (suma%10==0);
+    
+},
+
+  maskify: function (creditCard){
+    
+    let michi="";
+
+    for (let i = 0; i <creditCard.length; i++ ){
+      if(i<creditCard.length-4){
+        michi = michi + "#";
+      } else{
+        michi = michi+ creditCard[i];
+      }
+  
+     }
+
+     return michi;
+  }
+}
+
 
 export default validator;
